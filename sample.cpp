e87083aab2e9c1998a059aa81a34aa279f8f1363
Brianching522 <brianching522@gmail.com>
2022-03-07 17:29:55 +0800

    curve along curve with pre defined pts

--- sample.cpp
+++ sample.cpp
@@ -111,16 +111,24 @@ void SampleModel::draw()
 		}
 		else
 		{
+			num_point = 3;
+
 			ctrl = new Point[int(VAL(NOP))];
-			for(int i = 0; i < int(VAL(NOP)); ++i)
-				ctrl[i].x = double(i);
+			// for(int i = 0; i < int(VAL(NOP)); ++i)
+			// 	ctrl[i].x = double(i);
+			ctrl[0] = Point(0, 0, 0);
+			ctrl[1] = Point(1, 1, 0);
+			ctrl[2] = Point(2, 0, 0);
 
 			ctrl2 = new Point[int(VAL(NOP))];
-			for(int i = 0; i < int(VAL(NOP)); ++i)
-				ctrl2[i].x = double(i);
+			// for(int i = 0; i < int(VAL(NOP)); ++i)
+			// 	ctrl2[i].x = double(i);
+			ctrl2[0] = Point(0, 0, 0);
+			ctrl2[1] = Point(1, -1, 0);
+			ctrl2[2] = Point(2, 0, 0);
 		}
 
-		num_point = VAL(NOP);
+		// num_point = VAL(NOP);
 
 		pts = new Point*[num_point];
 		for(int i = 0; i < num_point; ++i)
@@ -174,18 +182,23 @@ void SampleModel::draw()
 				}
 			}
 
-		// draw_pts = new Point* [100];
-		// for(int i = 0; i < 100; ++i)
-		// {
-		// 	draw_pts[i] = new Point[100];
-		// 	for(int j = 0; j < 100; ++j)
-		// 	{
-		// 		double x = 1, y = 0, z = 0,
-		// 			   x1 = x *cos(atan(dzpts(99))), z1 = -x*sin(atan(dzpts(99))),  // y axis rotate
-		// 			   theta = atan(dypts(99)),
-		// 			   x2 = x1 *cos(theta), y2 = x1*sin(theta);  //z axis rotate
-		// 	}
-		// }
+		draw_pts = new Point* [100];
+		for(int i = 0; i < 100; ++i)
+		{
+			draw_pts[i] = new Point[100];
+			for(int j = 0; j < 100; ++j)
+			{
+				double x = pts[0][j].x, y = pts[0][j].y, z = pts[0][j].z,
+					   x1 = x, y1 = y*cos(atan(dypath(i))) - z*sin(atan(dypath(i))), z1 = y*sin(atan(dypath(i)))+ z*cos(atan(dypath(i))),  // x axis rotate
+					   theta = atan(dzpath(i)),
+					   x2 = x1*cos(theta)+ z1*sin(theta), y2 = y1, z2 = -x1*sin(theta)+ z1*cos(theta);  //y axis rotate
+				
+				//printf("%1f %1f\n", atan(dypath(j)), theta);
+				draw_pts[i][j].x = x2+path[0][i].z;
+				draw_pts[i][j].y = y2+path[0][i].y;
+				draw_pts[i][j].z = z2-path[0][i].x;
+			}
+		}
 	}
 
 	glPushMatrix();
@@ -209,6 +222,17 @@ void SampleModel::draw()
 		glVertex3d(pts[0][i].x, pts[0][i].y, pts[0][i].z);
 	glEnd();
 
+	for(int i = 0; i < 99; ++i)
+	{
+		glBegin(GL_TRIANGLE_STRIP);
+		for(int j = 0; j < 100; ++j)
+		{
+			glVertex3d(draw_pts[i][j].x, draw_pts[i][j].y, draw_pts[i][j].z);
+			glVertex3d(draw_pts[i+1][j].x, draw_pts[i+1][j].y, draw_pts[i+1][j].z);
+		}
+		glEnd();
+	}
+
 }
 
 int main()
@@ -222,7 +246,7 @@ int main()
     controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
     controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
 	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
-	controls[NOP] = ModelerControl("Num", 2, 6, 1, 2);
+	controls[NOP] = ModelerControl("Num", 2, 6, 1, 4);
 	controls[P] = ModelerControl("Points", 0, 8, 1, 0);
 	controls[C] = ModelerControl("Circle", 0, 1, 1, 1);
 	controls[X] = ModelerControl("NewX", -4, 4, 1, 0);
